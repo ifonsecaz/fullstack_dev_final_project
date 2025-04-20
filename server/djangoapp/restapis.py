f97737a7d46dcba204212a0beca6d927cb6dff3c
Israel Fonseca Zárate <ifonsecap351@gmail.com>
2025-04-19 23:37:20 -0600

    sentAnalysis

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -33,11 +33,20 @@ def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of requests library with URL and parameters
+        print("peticion")
+        print(request_url)
         response = requests.get(request_url)
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
-# def post_review(data_dict):
-# Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
+
