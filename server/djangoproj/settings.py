4a399105debbf08a9887eef9bfa4a7f2f00be66b
Israel Fonseca Zárate <ifonsecap351@gmail.com>
2025-04-20 00:43:56 -0600

    syntax errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','https://zerueltypexy-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/','https://zerueltypexy-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 
+                 'https://zerueltypexy-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000/', 
+                        'https://zerueltypexy-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
